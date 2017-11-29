8db5256181388269265adf53f1318f678d59c076
Hannah Danziger <hdanziger8@gmail.com>
2017-11-29 20:20:23 +0000

    first commit

---
+++ lib/Read/Books/CRUD.rb
@@ -0,0 +1,9 @@
+require "Read/Books/CRUD/version"
+
+module Read
+  module Books
+    module CRUD
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/Read/Books/CRUD/version.rb
@@ -0,0 +1,7 @@
+module Read
+  module Books
+    module CRUD
+      VERSION = "0.1.0"
+    end
+  end
+end
